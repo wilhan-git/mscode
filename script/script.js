82b1c69bbef5b180c20772d980278f89b810c118
wilhan <Wilhan Pereira>
2022-12-30 23:08:20 -0300

    Adicionando mensagem da index

--- script/script.js
+++ script/script.js
@@ -1,16 +1,11 @@
-var header =document.getElementById('header');
-var navigationHeader = document.getElementById('navgation_head');
-var content= document.getElementById('content');
+let menuMobile = document.querySelector('.mobile-menu');
 
-var showSidebar = false;
+function menuShow(){
+    menuMobile.classList.add('open');
+    
+   
+}
 
-function toggleSidebar (){
-    showSidebar = !showSidebar;
-    if(showSidebar){
-        navigationHeader.style.marginLeft= '-10vw';
-        navigationHeader.style.animation ='showSidebar';
-    } else{
-        navigationHeader.style.marginLeft= '-100vw';
-        navigationHeader.style.animation ='';
-    }
+function menuClose(){
+    menuMobile.classList.remove('open');
 }
