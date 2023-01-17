3e58d1b4e3c646abe6ef689721057e692e2c4949
wilhan <Wilhan Pereira>
2023-01-16 22:02:19 -0300

    acertando aparte mobile

--- script/script.js
+++ script/script.js
@@ -1,11 +1,14 @@
 let menuMobile = document.querySelector('.mobile-menu');
+let menuicon = document.querySelector('.mobile-menu-icon');
 
 function menuShow(){
     menuMobile.classList.add('open');
+    menuicon.style.display = 'none';
     
    
 }
 
 function menuClose(){
     menuMobile.classList.remove('open');
+    menuicon.style.display = '';
 }
